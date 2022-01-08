9129caa6065d2365dfe8286ecfffb6ca8a327771
ljoonal <ljoonal@ljoonal.xyz>
2022-01-08 10:59:18 +0200

    Add UserSession request builder

--- src/api_client/req_models.rs
+++ src/api_client/req_models.rs
@@ -36,3 +36,77 @@ pub struct NeosRequestUserSession {
 	/// time.
 	pub remember_me: bool,
 }
+
+impl NeosRequestUserSession {
+	#[must_use]
+	/// Creates a user session request with a certain identifier
+	pub fn with_identifier(
+		identifier: impl Into<NeosRequestUserSessionIdentifier>,
+		password: impl Into<String>,
+	) -> NeosRequestUserSession {
+		Self {
+			identifier: identifier.into(),
+			password: password.into(),
+			totp: None,
+			secret_machine_id: None,
+			remember_me: false,
+		}
+	}
+
+	#[must_use]
+	/// Creates a user session request with an username.
+	pub fn with_username(
+		username: impl Into<String>,
+		password: impl Into<String>,
+	) -> NeosRequestUserSession {
+		Self::with_identifier(
+			NeosRequestUserSessionIdentifier::Username(username.into()),
+			password,
+		)
+	}
+
+	#[must_use]
+	/// Creates a user session request from an email.
+	pub fn with_email(
+		email: impl Into<String>,
+		password: impl Into<String>,
+	) -> NeosRequestUserSession {
+		Self::with_identifier(
+			NeosRequestUserSessionIdentifier::Email(email.into()),
+			password,
+		)
+	}
+
+	#[must_use]
+	/// Creates a user session request from the owner's ID.
+	pub fn with_owner_id(
+		owners_id: impl Into<String>,
+		password: impl Into<String>,
+	) -> NeosRequestUserSession {
+		Self::with_identifier(
+			NeosRequestUserSessionIdentifier::OwnerID(owners_id.into()),
+			password,
+		)
+	}
+
+	#[must_use]
+	/// Sets the totp field's value
+	pub fn totp(mut self, totp: impl Into<Option<String>>) -> Self {
+		self.totp = totp.into();
+		self
+	}
+
+	#[must_use]
+	/// Sets the totp field's value
+	pub fn machine_id(mut self, machine_id: impl Into<Option<String>>) -> Self {
+		self.secret_machine_id = machine_id.into();
+		self
+	}
+
+	#[must_use]
+	/// Sets the totp field's value
+	pub fn remember_me(mut self, remember_me: impl Into<bool>) -> Self {
+		self.remember_me = remember_me.into();
+		self
+	}
+}
