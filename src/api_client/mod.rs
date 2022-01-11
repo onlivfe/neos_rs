a959511c9d87e8fa5e2b822d5d5ed6502490946c
ljoonal <ljoonal@ljoonal.xyz>
2022-01-11 15:49:26 +0200

    Cleanup the doc examples to be shorter

--- src/api_client/mod.rs
+++ src/api_client/mod.rs
@@ -57,7 +57,7 @@ use crate::{NeosGroup, NeosSession, NeosUser, NeosUserStatus};
 /// Counts the amount of users that are active in publicly listed sessions
 ///
 /// ```no_run
-/// use neos::api_client::{Neos, NeosUnauthenticated};
+/// # use neos::api_client::{Neos, NeosUnauthenticated};
 /// # let USER_AGENT = String::new();
 /// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
 /// let sessions = neos_api_client.get_sessions();
@@ -76,7 +76,7 @@ use crate::{NeosGroup, NeosSession, NeosUser, NeosUserStatus};
 /// ```
 pub trait Neos {
 	#[doc(hidden)]
-	// Meant for internal use only.
+	/// Meant for internal use only.
 	fn api_request(
 		&self,
 		method: Method,
@@ -89,9 +89,9 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
 	/// if neos_api_client.ping().is_ok() {
 	/// 	println!("Neos is reachable :)");
 	/// } else {
@@ -108,18 +108,13 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
-	/// let online_users_count = neos_api_client.online_user_count();
-	/// match online_users_count {
-	/// 	Ok(online_users_count) => {
-	/// 		println!("Neos currently has {} online users", online_users_count);
-	/// 	}
-	/// 	Err(err) => {
-	/// 		println!("Couldn't get the online users count: {} ", err);
-	/// 	}
-	/// };
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// let online_users_count = neos_api_client
+	/// 	.online_user_count()
+	/// 	.expect("to be able to get the online user count from Neos");
+	/// println!("Neos currently has {} online users", online_users_count);
 	/// ```
 	fn online_user_count(&self) -> Result<u32, RequestError> {
 		let resp = self.api_request(Method::Get, "stats/onlineUsers", &mut Ok)?;
@@ -137,18 +132,13 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
-	/// let online_instance_count = neos_api_client.online_instance_count();
-	/// match online_instance_count {
-	/// 	Ok(online_instance_count) => {
-	/// 		println!("Neos currently has {} online instances", online_instance_count);
-	/// 	}
-	/// 	Err(err) => {
-	/// 		println!("Couldn't get the online instance count: {} ", err);
-	/// 	}
-	/// };
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// let online_instance_count = neos_api_client
+	/// 	.online_instance_count()
+	/// 	.expect("to be able to get the online instance count from Neos");
+	/// println!("Neos currently has {} online instances", online_instance_count);
 	/// ```
 	fn online_instance_count(&self) -> Result<u32, RequestError> {
 		let resp = self.api_request(Method::Get, "stats/onlineInstances", &mut Ok)?;
@@ -166,18 +156,20 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
-	/// let sessions = neos_api_client.get_sessions();
-	/// match sessions {
-	/// 	Ok(sessions) => {
-	/// 		println!("{} public sessions", sessions.len());
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// let sessions = neos_api_client
+	/// 	.get_sessions()
+	/// 	.expect("to be able to get publicly visible sessions from Neos");
+	/// for session in sessions {
+	/// 	if session.active_users > 0 {
+	/// 		println!(
+	/// 			"Session {} has {} active users",
+	/// 			session.name, session.active_users
+	/// 		);
 	/// 	}
-	/// 	Err(err) => {
-	/// 		println!("Couldn't get the session details: {} ", err);
-	/// 	}
-	/// };
+	/// }
 	/// ```
 	fn get_sessions(&self) -> Result<Vec<NeosSession>, RequestError> {
 		let resp = self.api_request(Method::Get, "sessions", &mut Ok)?;
@@ -190,18 +182,13 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
-	/// let neos_bot = neos_api_client.get_user("Neos");
-	/// match neos_bot {
-	/// 	Ok(neos_bot) => {
-	/// 		println!("Fetched the account of {}", &neos_bot.username);
-	/// 	}
-	/// 	Err(err) => {
-	/// 		println!("Couldn't get the session details: {} ", err);
-	/// 	}
-	/// };
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// let neos_bot = neos_api_client
+	/// 	.get_user("Neos")
+	/// 	.expect("to be able to get the Neos bot account from Neos");
+	/// println!("The Neos bot supposedly registered on {}", &neos_bot.registration_date);
 	/// ```
 	fn get_user(
 		&self,
@@ -224,11 +211,16 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
-	/// let matching_users = neos_api_client.search_users("Neos").unwrap();
-	/// let neos_bot = matching_users.iter().find(|user| user.username == "Neos").unwrap();
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// let matching_users = neos_api_client
+	/// 	.search_users("Neos")
+	/// 	.expect("to be able to get search for the Neos bot account from Neos");
+	/// let neos_bot = matching_users
+	/// 	.iter()
+	/// 	.find(|user| user.username == "Neos")
+	/// 	.expect("for the search results to contain the Neos bot account");
 	/// println!("Fetched the account of {}", &neos_bot.username);
 	/// ```
 	fn search_users(&self, name: &str) -> Result<Vec<NeosUser>, RequestError> {
@@ -243,19 +235,13 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
 	/// let neos_bot_status = neos_api_client
-	/// 	.get_user_status(neos::id::User::try_from("U-neos".to_string()).unwrap());
-	/// match neos_bot_status {
-	/// 	Ok(neos_bot_status) => {
-	/// 		println!("Neos bot account is: {}", &neos_bot_status.online_status);
-	/// 	}
-	/// 	Err(err) => {
-	/// 		println!("Couldn't get the session details: {} ", err);
-	/// 	}
-	/// };
+	/// 	.get_user_status(neos::id::User::try_from("U-neos").unwrap())
+	/// 	.expect("to be able to get the Neos bot account from Neos");
+	/// println!("Neos bot account is: {}", &neos_bot_status.online_status);
 	/// ```
 	fn get_user_status(
 		&self,
@@ -275,20 +261,13 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
-	/// let session = neos_api_client.get_session(
-	/// 	neos::id::Session::try_from("S-totally-legit-id".to_string()).unwrap(),
-	/// );
-	/// match session {
-	/// 	Ok(session) => {
-	/// 		println!("Session has {} active_users users", &session.active_users);
-	/// 	}
-	/// 	Err(err) => {
-	/// 		println!("Couldn't get the session details: {} ", err);
-	/// 	}
-	/// };
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// let session = neos_api_client
+	/// 	.get_session(neos::id::Session::try_from("S-totally-legit-id").unwrap())
+	/// 	.expect("to be able to get a session from the invalid id from Neos... yeh no");
+	/// println!("Session has {} active_users users", &session.active_users);
 	/// ```
 	fn get_session(
 		&self,
@@ -308,23 +287,17 @@ pub trait Neos {
 	/// # Example usage
 	///
 	/// ```no_run
-	/// use neos::api_client::{Neos, NeosUnauthenticated};
+	/// # use neos::api_client::{Neos, NeosUnauthenticated};
 	/// # let USER_AGENT = String::new();
-	/// let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
+	/// # let neos_api_client = NeosUnauthenticated::new(USER_AGENT);
 	/// let group = neos_api_client
-	/// 	.get_group(neos::id::Group::try_from("G-totally-legit-id".to_string()).unwrap());
-	/// match group {
-	/// 	Ok(group) => {
-	/// 		println!(
-	/// 			"The admin of the group {} is {}",
-	/// 			&group.name,
-	/// 			group.admin_user_id.as_ref()
-	/// 		);
-	/// 	}
-	/// 	Err(err) => {
-	/// 		println!("Couldn't get the session details: {} ", err);
-	/// 	}
-	/// };
+	/// 	.get_group(neos::id::Group::try_from("G-Neos").unwrap())
+	/// 	.expect("to be able to get the Neos group details from Neos");
+	/// println!(
+	/// 	"The admin of the group {} is {}",
+	/// 	&group.name,
+	/// 	group.admin_user_id.as_ref()
+	/// );
 	/// ```
 	fn get_group(&self, group_id: crate::id::Group) -> Result<NeosGroup, RequestError> {
 		let resp = self.api_request(
