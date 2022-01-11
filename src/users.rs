e659ccb83d5c37ce26823361e7378148c2f91fb6
ljoonal <ljoonal@ljoonal.xyz>
2022-01-11 14:24:14 +0200

    Handle errors on NeosUserPatreonData Dates

--- src/users.rs
+++ src/users.rs
@@ -217,11 +217,19 @@ pub struct NeosUserPatreonData {
 	pub last_is_anorak: bool,
 	/// The ID of the github issue that this user has set as their priority
 	pub priority_issue: u32,
+	#[serde(with = "serde_with::rust::default_on_error")]
 	/// Guess: The second last time when the user last activated their Patreon
 	/// subscription
-	pub last_plus_activation_time: DateTime<Utc>,
+	///
+	/// Wrong/Invalid dates such as `0001-01-01T00:00:00` are expressed as
+	/// None.
+	pub last_plus_activation_time: Option<DateTime<Utc>>,
+	#[serde(with = "serde_with::rust::default_on_error")]
 	/// When the user last activated their Patreon subscription
-	pub last_activation_time: DateTime<Utc>,
+	///
+	/// Wrong/Invalid dates such as `0001-01-01T00:00:00` are expressed as
+	/// None.
+	pub last_activation_time: Option<DateTime<Utc>>,
 }
 
 /// The amount of credits that a neos user has.
