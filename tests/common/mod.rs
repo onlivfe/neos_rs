535d8f13963a260da36c70edcae6381649195af9
ljoonal <ljoonal@ljoonal.xyz>
2022-01-11 12:59:54 +0200

    Have user session available to tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -18,12 +18,17 @@ const USER_AGENT: &str = concat!(
 pub static UNAUTHENTICATED_API_CLIENT: Lazy<NeosUnauthenticated> =
 	Lazy::new(|| NeosUnauthenticated::new(USER_AGENT.to_string()));
 
-pub static AUTHENTICATED_API_CLIENT: Lazy<NeosAuthenticated> = Lazy::new(|| {
+pub static USER_SESSION: Lazy<NeosUserSession> = Lazy::new(|| {
 	let user_session: NeosUserSession = serde_json::from_slice(
 		&std::fs::read("user-session.json")
 			.expect("must have a prepared `user-session.json` file for live API testing"),
 	)
 	.expect("`user-session.json` file to parse into a user session");
 
-	UNAUTHENTICATED_API_CLIENT.clone().upgrade(user_session)
+	assert!(user_session.secret_machine_id.is_some());
+
+	user_session
 });
+
+pub static AUTHENTICATED_API_CLIENT: Lazy<NeosAuthenticated> =
+	Lazy::new(|| UNAUTHENTICATED_API_CLIENT.clone().upgrade(USER_SESSION.clone()));
