05470821a78989335973759d3bd4fc58dc9961fb
ljoonal <ljoonal@ljoonal.xyz>
2023-02-24 20:05:31 +0200

    Forbid unsafe instead of just deny

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@
 
 #![cfg_attr(nightly, feature(doc_cfg))]
 #![deny(clippy::all)]
-#![deny(unsafe_code)]
+#![forbid(unsafe_code)]
 #![deny(clippy::cargo)]
 #![warn(missing_docs)]
 #![deny(rustdoc::invalid_html_tags)]
@@ -41,7 +41,7 @@
 // My project my choice, tabs are literally made for indentation, spaces not.
 #![allow(clippy::tabs_in_doc_comments)]
 
-/// The API's base path
+/// The base path of the API
 const API_BASE_URI: &str = "https://api.neos.com/api";
 
 pub mod id;
